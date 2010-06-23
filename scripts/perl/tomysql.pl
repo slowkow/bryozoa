5fd5c75111c0282f134dfd955875cac5446d8832
Kamil Slowikowski <kslowikowski@gmail.com>
2010-06-23 14:51:25 -0500

    shorten code

--- scripts/perl/tomysql.pl
+++ scripts/perl/tomysql.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 # Author       : Kamil Slowikowski <kslowikowski@gmail.com>
-# Version      : 0.1
+# Version      : 0.2
 # Date         : June 23, 2010
 # Instructions : To use this script, export a tab-delimited file from FileMaker
 #                and run this script on the tab-delimited file.
@@ -17,10 +17,10 @@ open(my $file, "<", $ARGV[0]) or die $!;
 
 while (<$file>) {
   # skip empty lines
-  next if (/^\s*$/);
+  next if /^\s*$/;
   # empty values should be marked \N
-  while ($_ =~ s/\t\t/\t\\N\t/g) {};
+  while (s/\t\t/\t\\N\t/g) {};
   # dates should be YYYY-MM-DD
-  while ($_ =~ s@\t(\d{1,2})/(\d{1,2})/(\d{2,4})\t@\t$3-$2-$1\t@g) {};
+  while (s@\t(\d{1,2})/(\d{1,2})/(\d{2,4})\t@\t$3-$2-$1\t@g) {};
   print;
 }
