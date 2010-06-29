8346554a04a11ce0686df0fc31ff3b7bc97a6b91
Kamil Slowikowski <kslowikowski@gmail.com>
2010-06-29 10:10:05 -0500

    added join method

--- scripts/mysql/bock_combine.sql
+++ scripts/mysql/bock_combine.sql
@@ -1,37 +1,51 @@
+/* Just use the PHP script*/
+
 /*
 let's use a database called bock
 */
+/*
 USE bock;
 
-/*
-DELETE FROM bryozoans WHERE currentname > 99990;
+DROP TABLE IF EXISTS bryozoansref;
+CREATE TABLE bryozoansref AS SELECT id, name FROM bryozoans
+WHERE `id` IS NOT NULL
+AND `name` IS NOT NULL;
+
+SELECT
+  bryozoans.name, bryozoans.id, bryozoans.currentname,
+  bryozoansref.id, bryozoansref.name
+FROM
+  bryozoans
+RIGHT JOIN bryozoansref ON
+  bryozoans.currentname = bryozoansref.id;
+
+DROP TABLE bryozoansref;
+
+QUIT
 */
 
 /*
-Add the currentnamestring column
-*/
-/*
-ALTER TABLE bryozoans ADD currentnamestring VARCHAR(512);
-*/
-
-
-/*
-Somehow, the PHP script performs this task much more quickly than pure MySQL.
+PHP script performs this task more quickly than pure MySQL if the ids are not
+indexed.
 Also, PHP gives more control over exactly what happens.
 */
-SELECT
-  /*bryozoans.name, bryozoans.id, bryozoans.currentname,
-  bryozoansref.id, bryozoansref.name*/
-  bryozoansref.name
-FROM
-  bryozoans,
-  (SELECT bryozoans.id, bryozoans.name FROM bryozoans) AS bryozoansref
-WHERE (
-  bryozoans.currentname IS NOT NULL
-  AND bryozoans.currentname < 99990
-  AND bryozoans.currentname = bryozoansref.id
-);
 
+/* Add the currentnamestring column */
+ALTER TABLE bryozoans ADD currentnamestring VARCHAR(512);
+/*
+SELECT
+  #bryozoans.name, bryozoans.id, bryozoans.currentname,
+  #bryozoansref.id, bryozoansref.name
+  `bryozoansref`.`name`
+FROM
+  `bryozoans`,
+  (SELECT `bryozoans`.`id`, `bryozoans`.`name` FROM `bryozoans`) AS `bryozoansref`
+WHERE (
+  `bryozoans`.`currentname` IS NOT NULL
+  AND `bryozoans`.`currentname` < 99990
+  AND `bryozoans`.`currentname` = `bryozoansref`.`id`
+);
+*/
 
 /*
 This is a function for adding a column to a table iff it doesn't exist.
