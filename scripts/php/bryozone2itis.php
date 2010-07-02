c371cd9806e485ed2eabdf004d6e422d32e4e138
Kamil Slowikowski <kslowikowski@gmail.com>
2010-07-02 17:33:28 -0500

    increased modularity, added graphviz code

--- scripts/php/bryozone2itis.php
+++ scripts/php/bryozone2itis.php
@@ -25,6 +25,39 @@ unit_name1	rank_name	parent_name	usage
 bryozoa	Phylum	animalia	valid
  */
 
+/**
+ * input a taxonid
+ * output the contents of the row with the taxonid
+ */
+function getRow($taxonid) {
+  $query = sprintf("SELECT * FROM `bryozone_taxa` WHERE `taxonid`='%s'",
+    mysql_real_escape_string($taxonid)
+  );
+  return mysql_fetch_array(mysql_query($query), MYSQL_ASSOC);
+}
+
+/**
+ * input the rank code
+ * output the rank name
+ */
+function getRankName($rankcode) {
+  $query = sprintf("SELECT `rankname` FROM `bryozone_rank` WHERE `rankid`='%s'",
+    mysql_real_escape_string($rankcode)
+  );
+  $row = mysql_fetch_array(mysql_query($query), MYSQL_ASSOC);
+  return $row['rankname'];
+}
+
+/**
+ * input the rank code
+ * output true/false if we should print it
+ */
+function isValidRankCode($rank_code) {
+  return 3 < $rank_code && $rank_code < 110 && $rank_code != 60
+    && $rank_code != 36 && $rank_code != 85 && $rank_code != 95
+    && $rank_code != 96 && $rank_code != 97 && $rank_code != 98;
+}
+
 // connect to localhost
 $link = mysql_connect('localhost', 'kamil');
 if (!$link) { die('Could not connect: ' . mysql_error()); }
@@ -32,7 +65,7 @@ if (!$link) { die('Could not connect: ' . mysql_error()); }
 $db_selected = mysql_select_db('bock', $link);
 if (!$db_selected) { die ('Could not use database: ' . mysql_error()); }
 
-// select id, name, and currentname for each row, if currentname is set
+// select some vars to put in row
 $result = mysql_query(
   "SELECT `taxonid`, `taxonname`, `parentid`, `rankcode`, `seniorid`"
   . " FROM `bryozone_taxa`"
@@ -45,41 +78,56 @@ $result = mysql_query(
 /*
  * Print the header
  */
-print("unit_name1\tunit_name2\trank_name\tparent_name\tusage\n");
+print("rank_name\tunit_name1\tunit_name2\tparent_name\tusage\n");
 
 // loop through results
 while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
-  // find the parent's name
-  $query = sprintf("SELECT `taxonname` FROM `bryozone_taxa` WHERE `taxonid`='%s'",
-    mysql_real_escape_string($row['parentid'])
-  );
-  $row2 = mysql_fetch_array(mysql_query($query), MYSQL_ASSOC);
-  $parent_name = $row2['taxonname'];
-  
-  $query = sprintf("SELECT `rankname` FROM `bryozone_rank` WHERE `rankid`='%s'",
-    mysql_real_escape_string($row['rankcode'])
-  );
-  $row3 = mysql_fetch_array(mysql_query($query), MYSQL_ASSOC);
-  $rank_name = $row3['rankname'];
-  
-  $unit_name1 = $row['taxonname'];
-  $usage = 'invalid';
-  
+  // get parent's name by looking at row's parentid
+  $parent_row = getRow($row['parentid']);
+  $parent_name = $parent_row['taxonname'];
+  $parent_rank_code = $parent_row['rankcode'];
+  $parent_rank_name = getRankName($parent_rank_code);
+  // get row's rank name by looking up row's rankcode
+  $rank_name = getRankName($row['rankcode']);
+  // get row's unit names directly
+  list($unit_name1, $unit_name2, $unit_name3, $unit_name4) =
+    explode(" ", $row['taxonname']);
+  // get validity by comparing the seniorid and the taxonid
   if ($row['seniorid'] && $row['seniorid'] == $row['taxonid']) {
     $usage = 'valid';
+  }
+  else {
+    $usage = 'invalid';
   }
   
   $rank_code = $row['rankcode'];
   
   // we got a name
-  if ($unit_name1 && $rank_name && $parent_name && $usage) {
+  if ($rank_name && $unit_name1 && $parent_name && $usage) {
+    if ($unit_name1 == 'Uncertain') {
+      continue;
+    }
+    
     if ($rank_code == 110) {
+      continue;
       //print("$parent_name\t$unit_name1\t$rank_name\t$parent_name\t$usage\n");
     }
-    else if (3 < $rank_code && $rank_code < 110 && $rank_code != 60
-    && $rank_code != 36 && $rank_code != 85 && $rank_code != 95
-    && $rank_code != 96 && $rank_code != 97 && $rank_code != 98) {
-     print("$unit_name1\t\t$rank_name\t$parent_name\t$usage\n");
+    
+    // find a parent that is named
+    while ($parent_name == 'Uncertain') {
+      $parent_row  = getRow($parent_row['parentid']);
+      $parent_name = $parent_row['taxonname'];
+      $parent_rank_code = $parent_row['rankcode'];
+      $parent_rank_name = getRankName($parent_rank_code);
+    }
+    
+    if (isValidRankCode($rank_code) && isValidRankCode($parent_rank_code)) {
+      print("$rank_name\t$rank_name $unit_name1\t$unit_name2\t$parent_rank_name $parent_name\t$usage\n");
+/*
+      // graphviz output
+      $name = implode(" ", array($unit_name1, $unit_name2));
+      print("\"$parent_rank_name $parent_name\" -> \"$rank_name $name\";\n");
+*/
     }
   }
 }
