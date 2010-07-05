7d86b336bbd52cbb1a3acbcca72ca3d2444c7684
Kamil Slowikowski <kslowikowski@gmail.com>
2010-07-04 21:56:17 -0500

    handle explicit cases of DD/MM/YYYY and MM/DD/YYYY

--- scripts/perl/tomysql.pl
+++ scripts/perl/tomysql.pl
@@ -21,6 +21,15 @@ while (<$file>) {
   # empty values should be marked \N
   while (s/\t\t/\t\\N\t/g) {};
   # dates should be YYYY-MM-DD
-  while (s@\t(\d{1,2})/(\d{1,2})/(\d{2,4})\t@\t$3-$2-$1\t@g) {};
+  #while (s@\t(\d{1,2})/(\d{1,2})/(\d{2,4})\t@\t$3-$2-$1\t@g) {};
+  while (m@\t(\d{1,2})/(\d{1,2})/(\d{2,4})\t@g) {
+    # catch explicit errors
+    if ($2 > 12) {
+      s@\t(\d{1,2})/(\d{1,2})/(\d{2,4})\t@\t$3-$1-$2\t@;
+    }
+    else {
+      s@\t(\d{1,2})/(\d{1,2})/(\d{2,4})\t@\t$3-$2-$1\t@;
+    }
+  };
   print;
 }
