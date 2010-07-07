563ee4681cce92c8bdfa86b59b3389aa6fce6c67
Kamil Slowikowski <kslowikowski@gmail.com>
2010-07-07 10:54:17 -0500

    copied bock_currentspecies.sql into bock_import.sql

--- scripts/mysql/bock_import.sql
+++ scripts/mysql/bock_import.sql
@@ -5,6 +5,11 @@ CREATE DATABASE IF NOT EXISTS `bock`;
 USE `bock`;
 
 /*
+we want to see a lot of errors if they exist
+*/
+SET max_error_count=1000;
+
+/*
 drop the bryozoans table if it exists already
 */
 DROP TABLE IF EXISTS `bryozoans`;
@@ -33,15 +38,50 @@ CREATE TABLE `bryozoans` (
   KEY (`currentname`)
 );
 
-/*
-we want to see a lot of errors if they exist
-*/
-SET max_error_count=1000;
-
+SELECT "Loading bryozoans" AS "Action";
 /*
 load the tab-delimited file into the database, ignore the header line
 */
 LOAD DATA LOCAL INFILE '../../bock/Jun2010/Bryozoans_mysql.tab' INTO TABLE `bryozoans` IGNORE 1 LINES;
+
+/*
+let's see those warnings
+*/
+SHOW WARNINGS;
+
+/*
+drop the currentspecies table if it exists already
+*/
+DROP TABLE IF EXISTS `currentspecies`;
+
+/*
+create a table with the proper data types for each field
+*/
+CREATE TABLE `currentspecies` (
+  `speciesid` INT,
+  `name` VARCHAR(512),
+  `author` VARCHAR(512),
+  `famcode` INT,
+  `recent` INT,
+  `remarks` VARCHAR(512),
+  `date_created` DATE,
+  `date_modified` DATE,
+  `first_name` VARCHAR(512),
+  `html_page` VARCHAR(2048),
+  `OK` INT,
+  `status` VARCHAR(512),
+  `familyname` VARCHAR(512),
+  PRIMARY KEY (`name`),
+  KEY (`speciesid`),
+  KEY (`famcode`)
+);
+
+
+SELECT "Loading currentspecies" AS "Action";
+/*
+load the tab-delimited file into the database, ignore the header line
+*/
+LOAD DATA LOCAL INFILE '../../bock/Jun2010/CURRENTSPECIES_mysql.tab' INTO TABLE `currentspecies` IGNORE 1 LINES;
 
 /*
 let's see those warnings
