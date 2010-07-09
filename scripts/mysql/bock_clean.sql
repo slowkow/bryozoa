e14513a00d3ac1d4af7bb1c788336cfe431d1f07
Kamil Slowikowski <kslowikowski@gmail.com>
2010-07-09 14:57:11 -0500

    add currentspecies statements uncomment bryozoans statements reformat file

--- scripts/mysql/bock_clean.sql
+++ scripts/mysql/bock_clean.sql
@@ -1,36 +1,89 @@
+/*
+Delete bad records and contradictory records.
+*/
+
 USE `bock`;
 
+/*******************************************************************************
+Table `bryozoans`
+  Number of records before deletion: 42202
+  Number of records after deletion:  34536
+  Number of records deleted:          7666
+*/
+
+DROP TABLE IF EXISTS `bryozoans_delete`;
+
 /*
-Delete records that Phil Bock marked for deletion.
+829 records
+Phil Bock marked these for deletion.
 */
 CREATE TABLE `bryozoans_delete`
-AS SELECT * FROM `bryozoans`
-WHERE `comments` LIKE '%delete%'
-OR `currentname` > 90000
-OR `name` LIKE '%comment%'
-OR `name` LIKE '%ignore%';
+  AS SELECT * FROM `bryozoans`
+    WHERE `comments` LIKE '%delete%' OR `currentname` > 90000
+      OR `name` LIKE '%comment%' OR `name` LIKE '%ignore%';
 
 DELETE FROM `bryozoans`
-WHERE `comments` LIKE '%delete%'
-OR `currentname` > 90000
-OR `name` LIKE '%comment%'
-OR `name` LIKE '%ignore%';
+  WHERE `comments` LIKE '%delete%' OR `currentname` > 90000
+    OR `name` LIKE '%comment%' OR `name` LIKE '%ignore%';
 
-/* Current_name = id but valid 0
-SELECT id, name, comments FROM bryozoans WHERE currentname = id AND valid = 0;
+/*
+3961 records
+Current_name = id but valid 0
+*/
+INSERT INTO `bryozoans_delete` SELECT * FROM `bryozoans`
+  WHERE `currentname` = `id` AND `valid` = 0;
+DELETE FROM `bryozoans` WHERE `currentname` = `id` AND `valid` = 0;
+
+/*
+1194 records
+Current_name null but valid 1
+*/
+INSERT INTO `bryozoans_delete` SELECT * FROM `bryozoans`
+  WHERE `currentname` IS NULL AND `valid` = 1;
+DELETE FROM `bryozoans` WHERE `currentname` IS NULL AND `valid` = 1;
+
+/*
+1988 records
+Non-alpha characters
+*/
+INSERT INTO `bryozoans_delete` SELECT * FROM `bryozoans`
+  WHERE `name` REGEXP "[^A-Za-z ]";
+DELETE FROM `bryozoans` WHERE `name` REGEXP "[^A-Za-z ]";
+
+/*
+9 records
+Valid 1 but status says synonym
+*/
+INSERT INTO `bryozoans_delete` SELECT * FROM `bryozoans`
+  WHERE `valid` = 1 AND `status` LIKE '%synonym%';
+DELETE FROM `bryozoans` WHERE `valid` = 1 AND `status` LIKE '%synonym%';
+
+/*******************************************************************************
+Table `currentspecies`
+  Number of records before deletion: 21039
+  Number of records after deletion:  19558
+  Number of records deleted:          1481
 */
 
-/* Current_name null but valid 1
-SELECT id, name, comments FROM bryozoans WHERE currentname IS NULL AND valid = 1;
-*/
+DROP TABLE IF EXISTS `currentspecies_delete`;
 
-/* Non-alpha characters
-SELECT name FROM bryozoans WHERE name REGEXP ".*[[\\(\\.&0-9'\"].*";
-
-More results with this query
-SELECT name FROM bryozoans WHERE name REGEXP "[^A-Za-z ]";
+/*
+88 records with OK=1 and status='Current'
+ 5 records with OK=1 and status!='Current'
+Delete the 5 records
 */
+CREATE TABLE `currentspecies_delete`
+  AS SELECT * FROM `currentspecies`
+    WHERE `OK` = 1 AND `status` NOT LIKE 'current';
 
-/* Valid 1 but status says synonym
-SELECT id, name, author, valid, status FROM bryozoans WHERE valid = 1 AND status LIKE '%synonym%';
+DELETE FROM `currentspecies`
+  WHERE `OK` = 1 AND `status` NOT LIKE 'current';
+
+/*
+1478 records
+Non-alpha characters
+(equals sign and numbers are ok because some records reference others)
 */
+INSERT INTO `currentspecies_delete` SELECT * FROM `currentspecies`
+  WHERE `name` REGEXP "[^A-Za-z =0-9]";
+DELETE FROM `currentspecies` WHERE `name` REGEXP "[^A-Za-z =0-9]";
