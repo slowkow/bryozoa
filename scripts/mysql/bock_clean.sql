184e5d535e46a8badfc4bf0341b1e042c6afe623
Kamil Slowikowski <kslowikowski@gmail.com>
2010-07-12 15:10:30 -0500

    delete `bryozoans` records that point to nonexistant records add keys to delete tables delete at the end rather than at each query

--- scripts/mysql/bock_clean.sql
+++ scripts/mysql/bock_clean.sql
@@ -7,56 +7,89 @@ USE `bock`;
 /*******************************************************************************
 Table `bryozoans`
   Number of records before deletion: 42202
-  Number of records after deletion:  34536
-  Number of records deleted:          7666
+  Number of records after deletion:  30694
+  Number of records deleted:         11508
 */
 
 DROP TABLE IF EXISTS `bryozoans_delete`;
 
+SELECT "Cleaning `bryozoans`" AS "Action";
+SELECT COUNT(*) FROM `bryozoans`;
+
 /*
 829 records
-Phil Bock marked these for deletion.
+Phil Bock marked these for extra work or deletion.
 */
 CREATE TABLE `bryozoans_delete`
   AS SELECT * FROM `bryozoans`
-    WHERE `comments` LIKE '%delete%' OR `currentname` > 90000
+    WHERE `comments` LIKE '%delete%' OR `currentname` > 90000 OR `id` > 90000
       OR `name` LIKE '%comment%' OR `name` LIKE '%ignore%';
+ALTER TABLE `bryozoans_delete`
+  ADD PRIMARY KEY (`name`),
+  ADD KEY (`id`),
+  ADD KEY (`currentname`);
 
-DELETE FROM `bryozoans`
-  WHERE `comments` LIKE '%delete%' OR `currentname` > 90000
-    OR `name` LIKE '%comment%' OR `name` LIKE '%ignore%';
+/*
+372
+Phil Bock marked these for extra work.
+SELECT COUNT(*) FROM `bryozoans` WHERE comments LIKE '%check%' OR comments LIKE '%?%';
+*/
 
 /*
 3961 records
 Current_name = id but valid 0
 */
-INSERT INTO `bryozoans_delete` SELECT * FROM `bryozoans`
+INSERT IGNORE INTO `bryozoans_delete` SELECT * FROM `bryozoans`
   WHERE `currentname` = `id` AND `valid` = 0;
-DELETE FROM `bryozoans` WHERE `currentname` = `id` AND `valid` = 0;
 
 /*
 1194 records
 Current_name null but valid 1
 */
-INSERT INTO `bryozoans_delete` SELECT * FROM `bryozoans`
+INSERT IGNORE INTO `bryozoans_delete` SELECT * FROM `bryozoans`
   WHERE `currentname` IS NULL AND `valid` = 1;
-DELETE FROM `bryozoans` WHERE `currentname` IS NULL AND `valid` = 1;
 
 /*
 1988 records
 Non-alpha characters
 */
-INSERT INTO `bryozoans_delete` SELECT * FROM `bryozoans`
+INSERT IGNORE INTO `bryozoans_delete` SELECT * FROM `bryozoans`
   WHERE `name` REGEXP "[^A-Za-z ]";
-DELETE FROM `bryozoans` WHERE `name` REGEXP "[^A-Za-z ]";
 
 /*
 9 records
 Valid 1 but status says synonym
 */
-INSERT INTO `bryozoans_delete` SELECT * FROM `bryozoans`
+INSERT IGNORE INTO `bryozoans_delete` SELECT * FROM `bryozoans`
   WHERE `valid` = 1 AND `status` LIKE '%synonym%';
-DELETE FROM `bryozoans` WHERE `valid` = 1 AND `status` LIKE '%synonym%';
+
+/*
+633 records
+Current_name points to a record that doesn't exist
+*/
+INSERT IGNORE INTO `bryozoans_delete` (
+  SELECT * FROM `bryozoans` AS `t1`
+    WHERE `currentname` IS NOT NULL
+      AND (
+        /* the referenced record does not exist in bryozoans */
+        NOT EXISTS (
+          SELECT * FROM `bryozoans` AS `t2` WHERE `t2`.`id` = `t1`.`currentname`
+        )
+        /* the referenced record has been deleted */
+        OR EXISTS (
+          SELECT * FROM `bryozoans_delete` AS `t3` WHERE `t3`.`id` = `t1`.`currentname`
+        )
+      )
+);
+
+/* Delete the records */
+DELETE `t1` FROM `bryozoans` AS `t1`, `bryozoans_delete` AS `t2`
+  WHERE `t1`.`name` = `t2`.`name`;
+
+SELECT "Finished cleaning `bryozoans`" AS "Action";
+SELECT COUNT(*) FROM `bryozoans`;
+SELECT "Records in `bryozoans_delete`" AS "Action";
+SELECT COUNT(*) FROM `bryozoans_delete`;
 
 /*******************************************************************************
 Table `currentspecies`
@@ -67,6 +100,9 @@ Table `currentspecies`
 
 DROP TABLE IF EXISTS `currentspecies_delete`;
 
+SELECT "Cleaning `currentspecies`" AS "Action";
+SELECT COUNT(*) FROM `currentspecies`;
+
 /*
 88 records with OK=1 and status='Current'
  5 records with OK=1 and status!='Current'
@@ -75,9 +111,10 @@ Delete the 5 records
 CREATE TABLE `currentspecies_delete`
   AS SELECT * FROM `currentspecies`
     WHERE `OK` = 1 AND `status` NOT LIKE 'current';
-
-DELETE FROM `currentspecies`
-  WHERE `OK` = 1 AND `status` NOT LIKE 'current';
+ALTER TABLE `currentspecies_delete`
+  ADD PRIMARY KEY (`name`),
+  ADD KEY (`speciesid`),
+  ADD KEY (`famcode`);
 
 /*
 1478 records
@@ -87,6 +124,14 @@ Non-alpha characters
 PROBLEM:
 Some of the "was Foo Bar=123" point to these deleted records.
 */
-INSERT INTO `currentspecies_delete` SELECT * FROM `currentspecies`
+INSERT IGNORE INTO `currentspecies_delete` SELECT * FROM `currentspecies`
   WHERE `name` REGEXP "[^A-Za-z =0-9]";
-DELETE FROM `currentspecies` WHERE `name` REGEXP "[^A-Za-z =0-9]";
+
+/* Delete the records */
+DELETE `t1` FROM `currentspecies` AS `t1`, `currentspecies_delete` AS `t2`
+  WHERE `t1`.`name` = `t2`.`name`;
+
+SELECT "Finished cleaning `currentspecies`" AS "Action";
+SELECT COUNT(*) FROM `currentspecies`;
+SELECT "Records in `currentspecies_delete`" AS "Action";
+SELECT COUNT(*) FROM `currentspecies_delete`;
