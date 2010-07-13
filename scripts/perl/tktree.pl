28f39057c2b76124bc383fcc3fbe65e0572634d4
Kamil Slowikowski <kslowikowski@gmail.com>
2010-07-13 16:46:09 -0500

    fix regex, add documentation

--- scripts/perl/tktree.pl
+++ scripts/perl/tktree.pl
@@ -1,10 +1,16 @@
 #!/usr/bin/perl -w
+# Author      : Kamil Slowikowski <kslowikowski@gmail.com>
+# Date        : July 13, 2010
+# Description : Read list of paths with terms delimited by '.'.
+#               Display a Tk Tree with expandable nodes.
+# Notes       : Tk methods from http://www.perlmonks.org/?node_id=712482
 use strict;
 use warnings;
 use Tk;
 use Tk::Tree;
+use Getopt::Long;
 
-die("please give me a file\n") unless $ARGV[0];
+$ARGV[0] or die("Display a Tk Tree.\nUsage: tktree.pl <file>\n");
 
 my $main = MainWindow->new(-title => "Bryozoa");
 my $tree = $main->ScrlTree(
@@ -18,16 +24,25 @@ $tree->pack(-fill => 'both', -expand => 1);
 
 open(my $file, "<", $ARGV[0]) or die $!;
 while (<$file>) {
+  # remove newline
   chomp;
-  /.+\.(.+)$/;
-  my $text = $1 || $_;
+  # grab last item in list delimited by periods
+  /(?:.+\.)?(.+)$/;
   #print($_ . "\t" . $1 . "\n");
-  $tree->add($_, -text => $text, -state => 'normal');
+  $tree->add($_, -text => $1, -state => 'normal');
+  # hide species
+  #~ if ($1 =~ /species/i) {
+    #~ $tree->setmode($_, 'close');
+    #~ $tree->hide('entry' => $_);
+  #~ }
+  #~ else {
+    #~ $tree->setmode($_, 'open');
+  #~ }
 }
 close($file);
 
-#closeTree ($tree);
-openTree ($tree);
+closeTree ($tree);
+#openTree ($tree);
 
 MainLoop;
 
