b0af2cc2c767f4d614860c2aaa3813a2dae7fdc8
Kamil Slowikowski <kslowikowski@gmail.com>
2010-07-20 16:40:33 -0500

    grab GNI authors for `scratchpads` entries that are missing authors rather than GNI authors for ALL Bryan's higher taxa
    note: all Bryan's taxa have been queried in GNI, results in `gni_bryan_taxa_authors`

--- scripts/php/bryanauthors.php
+++ scripts/php/bryanauthors.php
@@ -1,4 +1,13 @@
 <?php
+/*
+ * This grabs `scratchpads` entries without authors and finds the author from
+ * GNI.
+ * Puts the data in `gni_scratchpads_taxa_authors`.
+ * 
+ * TODO
+ *   Grab names from GNI for ALL of Bryan's taxa, so we can put missing
+ *   parentheses and check what GNI says compared to Bryan.
+ */
 // connect to localhost
 $link = mysql_connect('localhost', 'kamil');
 if (!$link) { die('Could not connect: ' . mysql_error()); }
@@ -7,14 +16,16 @@ $db_selected = mysql_select_db('bock', $link);
 if (!$db_selected) { die ('Could not use database: ' . mysql_error()); }
 
 // get author/year information from GNI
-mysql_query("DROP TABLE IF EXISTS `gni_taxa_authors`");
+mysql_query("DROP TABLE IF EXISTS `gni_scratchpads_taxa_authors`");
 mysql_query(
-  "CREATE TABLE `gni_taxa_authors` ("
+  "CREATE TABLE `gni_scratchpads_taxa_authors` ("
   . " `name` VARCHAR(64) NOT NULL"
   . ", `author` VARCHAR(64) NOT NULL"
   . ", KEY (`name`) )"
 );
 
+/*
+// look at all of bryan's taxa
 $result = mysql_query(
   "SELECT `name`, `rankcode`"
   . " FROM `bryan_valid`"
@@ -22,18 +33,26 @@ $result = mysql_query(
   . " `name` NOT REGEXP 'null|uncertain'" // no bad names
   . " AND `rankcode` < 110" // only taxa higher than Species
 );
+*/
+// look at the entries in `scratchpads` without authors
+$result = mysql_query(
+  "SELECT `unit_name1`"
+  . " FROM `scratchpads`"
+  . " WHERE"
+  . " `taxon_author` NOT REGEXP '[0-9]'"
+);
 
 $count = 0;
 $numresults = mysql_num_rows($result);
 while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
   $results = array();
   // use "uni:" to tell the service that we want higher taxa
-  $cmd = "../perl/querygni.pl -d -l -m -n 100 " . "uni:" . $row['name'];
+  $cmd = "../perl/querygni.pl -d -l -m -n 100 " . "uni:" . $row['unit_name1'];
   exec($cmd, $results);
-  print("Progress: " . ++$count . "/" . $numresults . "\t" . $row['name'] . "\n");
+  print("Progress: " . ++$count . "/" . $numresults . "\t" . $row['unit_name1'] . "\n");
   foreach ($results as $value) {
     list($name, $author) = explode("\t", $value);
-    $query = sprintf("INSERT INTO `gni_taxa_authors`"
+    $query = sprintf("INSERT INTO `gni_scratchpads_taxa_authors`"
       . " SET"
       . " `name`='%s',"
       . " `author`='%s'",
