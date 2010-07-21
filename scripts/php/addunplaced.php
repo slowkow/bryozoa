42dbbb1f0b0033d323a68708695130a6c5d8a853
Kamil Slowikowski <kslowikowski@gmail.com>
2010-07-21 11:22:02 -0500

    add script for inserting dummy taxa for unplaced taxa

---
+++ scripts/php/addunplaced.php
@@ -0,0 +1,68 @@
+<?php
+/*
+ * Read an ITIS-for-Scratchpads style file.
+ * Insert dummy "TAXONNAME Unplaced RANKNAME" entries wherever appropriate.
+ * Relink all appropriate entries.
+ * 
+ * For example, Bryozoa has some Families in it. That's not cool. The solution
+ * is to make an additional Class called "Bryozoa Unplaced Fam." and then put
+ * the Families in there.
+ */
+require 'include/connect.php';
+require 'include/scratchpads.php';
+
+$result = mysql_query(
+  "SELECT `full_name`, `taxon_author`"
+  . " FROM `scratchpads`"
+  . " WHERE `rank_name` NOT LIKE 'subspecies'"
+  . " AND `rank_name` NOT LIKE 'species'"
+  . " AND `rank_name` NOT LIKE 'genus'"
+);
+
+// loop through results
+while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+  $children_parent_name = trim($row['full_name'] . ' ' . $row['taxon_author']);
+  $children_ranks = getChildrenRanks($children_parent_name);
+  $children       = getChildren($children_parent_name);
+  // handle case when parent has children of various ranks
+  if (count($children_ranks) <= 1) {
+    continue;
+  }
+  
+  $dummy_rank_name   = $children_ranks[0];
+  $dummy_parent_name = trim($row['full_name'] . ' ' . $row['taxon_author']);
+  
+/*
+  print($row['full_name'] . "\n");
+  var_dump($children_ranks);
+*/
+
+  foreach (array_slice($children_ranks, 1) as $rank) {
+    $dummy_full_name = '~' . $row['full_name'] . ' ' . plural($rank);
+    // insert dummy
+    insertIntoScratchpads(
+      array(
+        'full_name'     => $dummy_full_name,
+        'rank_name'     => $dummy_rank_name,
+        'usage'         => 'invalid',
+        'unit_name1'    => '~' . $row['full_name'],
+        'unit_name2'    => plural($rank),
+        'parent_name'   => $dummy_parent_name,
+        'accepted_name' => $dummy_parent_name,
+        'comments'      => 'dummy taxon for unplaced ' . plural($rank),
+      )
+    );
+    // update children to point to this dummy as their new parent
+    foreach ($children as $child) {
+      if ($child['rank_name'] == $rank) {
+        // set dummy as the new parent
+        insertIntoScratchpads(
+          array(
+            'full_name'   => $child['full_name'],
+            'parent_name' => $dummy_full_name,
+          )
+        );
+      }
+    }
+  }
+}
