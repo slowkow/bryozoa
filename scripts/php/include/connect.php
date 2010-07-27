c82562a2ef2cae005563995b69251d593281b75a
Kamil Slowikowski <kslowikowski@gmail.com>
2010-07-26 21:37:33 -0500

    use new database bryozoa rather than bock

--- scripts/php/include/connect.php
+++ scripts/php/include/connect.php
@@ -6,5 +6,5 @@
 $link = mysql_connect('localhost', 'kamil');
 if (!$link) { die('Could not connect: ' . mysql_error()); }
 // make bock the current db
-$db_selected = mysql_select_db('bock', $link);
+$db_selected = mysql_select_db('bryozoa', $link);
 if (!$db_selected) { die ('Could not use database: ' . mysql_error()); }
