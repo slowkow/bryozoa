13aa7af6b6d94d08910ecceaf4caa24ce31b78eb
Kamil Slowikowski <kslowikowski@gmail.com>
2010-07-27 16:26:04 -0500

    increased legibility

--- scripts/mysql/describe_scratchpads.sql
+++ scripts/mysql/describe_scratchpads.sql
@@ -5,12 +5,25 @@ SOURCE db.sql;
 TEE /tmp/describe_scratchpads.log;
 DESCRIBE `scratchpads`;
 SELECT COUNT(*) FROM `scratchpads`;
-SELECT COUNT(*),`rank_name` FROM `scratchpads` GROUP BY `rank_name` ORDER BY FIELD(`rank_name`,'Phlylum','Class','Order','Suborder','Infraorder','Superfamily','Family','Genus','Species','Subspecies');
+SELECT COUNT(*),`rank_name` FROM `scratchpads` GROUP BY `rank_name`
+  ORDER BY
+    FIELD(`rank_name`,'Phlylum','Class','Order','Suborder','Infraorder',
+      'Superfamily','Family','Genus','Species','Subspecies');
 SELECT COUNT(*),`usage` FROM `scratchpads` GROUP BY `usage`;
-SELECT COUNT(*),`rank_name`,`usage` FROM `scratchpads` GROUP BY `rank_name`,`usage` ORDER BY FIELD(`rank_name`,'Phlylum','Class','Order','Suborder','Infraorder','Superfamily','Family','Genus','Species','Subspecies'), `usage` desc;
-SELECT COUNT(*) FROM `scratchpads` WHERE `usage` = 'invalid' AND LENGTH(`unacceptability_reason`) < 1;
-SELECT COUNT(*) FROM `scratchpads` WHERE `usage` = 'invalid' AND LENGTH(`unacceptability_reason`) > 1;
-SELECT COUNT(*),`parent_name` FROM `scratchpads` GROUP BY `parent_name` ORDER BY COUNT(*) DESC LIMIT 10;
-SELECT `full_name`,`taxon_author` FROM `scratchpads` WHERE `taxon_author` IS NULL OR `taxon_author` NOT REGEXP '[0-9]{4}' ORDER BY `full_name`;
-SELECT COUNT(*),`unacceptability_reason` FROM `scratchpads` WHERE LENGTH(`unacceptability_reason`) > 1 GROUP BY `unacceptability_reason`;
-QUIT;
+SELECT COUNT(*),`rank_name`,`usage` FROM `scratchpads`
+  GROUP BY `rank_name`,`usage`
+  ORDER BY
+    FIELD(`rank_name`,'Phlylum','Class','Order','Suborder','Infraorder',
+      'Superfamily','Family','Genus','Species','Subspecies'),
+    `usage` desc;
+SELECT COUNT(*) FROM `scratchpads`
+  WHERE `usage` = 'invalid' AND LENGTH(`unacceptability_reason`) < 1;
+SELECT COUNT(*) FROM `scratchpads`
+  WHERE `usage` = 'invalid' AND LENGTH(`unacceptability_reason`) > 1;
+SELECT COUNT(*),`parent_name` FROM `scratchpads`
+  GROUP BY `parent_name` ORDER BY COUNT(*) DESC LIMIT 10;
+SELECT `full_name`,`taxon_author` FROM `scratchpads`
+  WHERE `taxon_author` IS NULL OR `taxon_author` NOT REGEXP '[0-9]{4}'
+  ORDER BY `full_name`;
+SELECT COUNT(*),`unacceptability_reason` FROM `scratchpads`
+  WHERE LENGTH(`unacceptability_reason`) > 1 GROUP BY `unacceptability_reason`;
