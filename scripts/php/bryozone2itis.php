1f0bf62cd1971d62a70eeaa05f62e434496b7f37
Kamil Slowikowski <kslowikowski@gmail.com>
2010-07-29 19:55:09 -0500

    fix code dealing with species in bryozone2itis.php new output saved in bryozone_itis.tab

--- scripts/php/bryozone2itis.php
+++ scripts/php/bryozone2itis.php
@@ -28,36 +28,37 @@ unit_name1	rank_name	parent_name	usage
 bryozoa	Phylum	animalia	valid
  */
 
-/**
- * Query the bryozone_taxa table with a taxonid and return the associated row.
- * 
- * @param taxonid
- *   An id number of a taxon.
- * @return
- *   The MySQL row that matches the taxon id number.
- */
-function getRow($taxonid) {
-  $query = sprintf("SELECT * FROM `bryozone_taxa` WHERE `taxonid`='%s'",
-    mysql_real_escape_string($taxonid)
-  );
-  return mysql_fetch_array(mysql_query($query), MYSQL_ASSOC);
-}
+require 'include/connect.php';
 
-/**
- * Query the bryozone_rank table with a rank code and return the rank name.
- * 
- * @param rankcode
- *   A rank code number.
- * @return
- *   The name associated with the rank code number.
- */
-function getRankName($rankcode) {
-  $query = sprintf("SELECT `rankname` FROM `bryozone_rank` WHERE `rankid`='%s'",
-    mysql_real_escape_string($rankcode)
-  );
-  $row = mysql_fetch_array(mysql_query($query), MYSQL_ASSOC);
-  return $row['rankname'];
-}
+$ranknames = array(
+  0 => 'Invalid',
+  1 => 'Nomen Oblitum',
+  2 => 'Nomen Nudum',
+  3 => 'Uncertain Classification',
+  10 => 'Phylum',
+  20 => 'Class',
+  30 => 'Order',
+  36 => 'Subjective Junior Synonym',
+  40 => 'Suborder',
+  50 => 'Infraorder',
+  60 => 'Grade',
+  70 => 'Superfamily',
+  80 => 'Family',
+  85 => 'Family Synonym',
+  90 => 'Genus',
+  95 => 'Genus Synonym',
+  96 => 'Subjective Junior Synonym',
+  97 => 'Objective Junior Synonym',
+  98 => 'Homonym',
+  100 => 'Subgenus',
+  110 => 'Species',
+  113 => 'Uncertain Species',
+  115 => 'Species Synonym',
+  116 => 'Subjective Junior Synonym',
+  117 => 'Objective Junior Synonym',
+  118 => 'Homonym',
+  99999 => 'Error',
+);
 
 /**
  * Return true or false if the rank code number is not equal to some values.
@@ -83,31 +84,22 @@ function isValidRankCode($rank_code) {
   }
   return false;
 }
-
 /**
- * Return true if the row links all the way to Bryozoa via parentid.
+ * Query the bryozone_taxa table with a taxonid and return the associated row.
  * 
- * @param row
- *   A row from bryozoa_taxa.
- * @param print
- *   If true, then print the path from row to Bryozoa.
+ * @param taxonid
+ *   An id number of a taxon.
  * @return
- *   Return true if the row links all the way to Bryozoa via parentid.
+ *   The MySQL row that matches the taxon id number.
+ * 
+ * @see nextRealParent()
  */
-function linksToBryozoa($row, $print) {
-  if ($print) { $linkpath = getRankName($row['rankcode']) . ' ' . $row['taxonname']; }
-  while ($row['parentid']) {
-    $row = getRow($row['parentid']);
-    if ($print) { $linkpath = getRankName($row['rankcode']) . ' ' . $row['taxonname'] . " > " . $linkpath;}
-    if ($row['taxonname'] == 'Bryozoa') {
-      if ($print) { print($linkpath . "\n"); }
-      return true;
-    }
-  }
-  if ($print) { print("ERROR " . $linkpath . "\n"); }
-  return false;
+function getRow($taxonid) {
+  $query = sprintf("SELECT * FROM `bryozone_taxa` WHERE `taxonid`='%s'",
+    mysql_real_escape_string($taxonid)
+  );
+  return mysql_fetch_array(mysql_query($query), MYSQL_ASSOC);
 }
-
 /**
  * input a row
  * climb up to next parent until we find one that is not 'Uncertain'
@@ -117,31 +109,20 @@ function linksToBryozoa($row, $print) {
  * 
  * @param row
  *   A row from bryozoa_taxa.
- * @param print
- *   If true, then print the path from row to the parent.
  * @return
  *   Return the next parent that is not called 'Uncertain'.
+ * 
+ * @see getRow()
  */
-function nextRealParent($row, $print) {
-  if ($print) { $linkpath = getRankName($row['rankcode']) . ' ' . $row['taxonname']; }
+function nextRealParent($row) {
   while ($row['parentid']) {
     $row = getRow($row['parentid']);
-    if ($print) { $linkpath = getRankName($row['rankcode']) . ' ' . $row['taxonname'] . " > " . $linkpath; }
     if ($row['taxonname'] != 'Uncertain' && isValidRankCode($row['rankcode'])) {
-      if ($print) { print($linkpath . "\n"); }
       return $row;
     }
   }
-  if ($print) { print("ERROR " . $linkpath . "\n"); }
   return NULL;
 }
-
-// connect to localhost
-$link = mysql_connect('localhost', 'kamil');
-if (!$link) { die('Could not connect: ' . mysql_error()); }
-// make bock the current db
-$db_selected = mysql_select_db('bock', $link);
-if (!$db_selected) { die ('Could not use database: ' . mysql_error()); }
 
 // select some vars to put in row
 $result = mysql_query(
@@ -161,75 +142,49 @@ print("Phylum\tPhylum Bryozoa\t\t\t\tvalid\n");
 
 // loop through results
 while ($row = mysql_fetch_assoc($result)) {
+  // we don't care for invalid records at the moment
+  if (!isValidRankCode($row['rankcode'])) {
+    continue;
+  }
+  
   // get parent's name by looking at row's parentid
-  $parent_row = getRow($row['parentid']);
-  $parent_name = $parent_row['taxonname'];
+  $parent_row       = nextRealParent($row);
+  $parent_name      = $parent_row['taxonname'];
   $parent_rank_code = $parent_row['rankcode'];
-  $parent_rank_name = getRankName($parent_rank_code);
+  $parent_rank_name = $ranknames[$parent_rank_code];
+  
   // get row's rank name by looking up row's rankcode
-  $rank_name = getRankName($row['rankcode']);
-  // get row's unit names directly
-  list($unit_name1, $unit_name2, $unit_name3, $unit_name4) =
-    explode(" ", $row['taxonname']);
-  // get validity by comparing the seniorid and the taxonid
-  if ($row['seniorid'] && $row['seniorid'] == $row['taxonid']) {
-    $usage = 'valid';
-  }
-  else {
-    $usage = 'invalid';
-  }
-  
   $rank_code = $row['rankcode'];
+  $rank_name = $ranknames[$rank_code];
   
-  // we got a name
+  // we have a species or subspecies without a genus name, not useful
+  if ($rank_code >= 110 && $parent_rank_name != 'Genus') {
+    continue;
+  }
+  
+  // it's a species or subspecies, so the genus name is unit_name1
+  if ($rank_code >= 110) {
+    $unit_name1 = $parent_name;
+    list($unit_name2, $unit_name3) =
+      explode(" ", $row['taxonname'], 2);
+  }
+  // it's a genus or higher taxon, so the name is more simple
+  else {
+    list($unit_name1, $unit_name2, $unit_name3) =
+      explode(" ", $row['taxonname'], 3);
+  }
+  // quit if it's an uncertain row
+  if ($unit_name1 == 'Uncertain') {
+    continue;
+  }
+  
+  // get validity by comparing the seniorid and the taxonid
+  $usage = $row['seniorid'] == $row['taxonid'] ? 'valid' : 'invalid';
+  
+  // we have what we need
   if ($rank_name && $unit_name1 && $parent_name && $usage) {
-    if ($unit_name1 == 'Uncertain') {
-      continue;
-    }
-    
-    if (!isValidRankCode($rank_code)) {
-      continue;
-    }
-    
-/*
-    if ($rank_code >= 90) {
-      continue;
-      //print("$parent_name\t$unit_name1\t$rank_name\t$parent_name\t$usage\n");
-    }
-*/
-    
-    // find a parent that is named
-    if ($parent_name == 'Uncertain' || !isValidRankCode($parent_rank_code)) {
-      $parent_row = nextRealParent($row, FALSE);
-      $parent_name = $parent_row['taxonname'];
-      $parent_rank_code = $parent_row['rankcode'];
-      $parent_rank_name = getRankName($parent_rank_code);
-    }
-
-    // check if this child links all the way back to Bryozoa
-    // they all link back, so this is not necessary
-/*
-    if (!linksToBryozoa($row, FALSE))
-    {
-      continue;
-    }
-*/
-    
-    if ($parent_row == NULL) {
-      continue;
-    }
-    
-    // if you don't print for "valid" codes, then you will have gaps
-    // between the taxon and Bryozoa
-    // TODO make nextRealParent and linksToBryozoa return FALSE
-    // if they don't find a valid ITIS rank
-    
-    print("$rank_name\t$rank_name $unit_name1\t$unit_name2\t$unit_name3\t$parent_rank_name $parent_name\t$usage\n");
-/*
-    // graphviz output
-    $name = trim(implode(" ", array($unit_name1, $unit_name2)));
-    print("\"$parent_rank_name $parent_name\" -> \"$rank_name $name\";\n");
-*/
+    //print("$rank_name\t$rank_name $unit_name1\t$unit_name2\t$unit_name3\t$parent_rank_name $parent_name\t$usage\n");
+    print("$rank_name\t$unit_name1\t$unit_name2\t$unit_name3\t$parent_name\t$usage\n");
   }
 }
 mysql_free_result($result);
