49cac20cc12099a9941616bde2e0649f785d7edb
Kamil Slowikowski <kslowikowski@gmail.com>
2010-07-31 21:51:52 -0500

    remove hardcoded path add progress messages rename checkitis.pl to itis2.pl

--- scripts/perl/checkallitis.sh
+++ scripts/perl/checkallitis.sh
@@ -1,9 +1,16 @@
-cd ~/Dropbox/fieldmuseum/bryozoa/scripts/perl
-./checkitis.pl -o f -r ../mysql/output/scratchpads.tab > output/scratchpads_fullhierarchy.txt
-./checkitis.pl -o p ../mysql/output/scratchpads.tab > output/scratchpads_uniquepaths.txt
+echo "Translating scratchpads to newick..."
+./itis2.pl -o n ../mysql/output/scratchpads.tab > output/scratchpads.newick
+echo "Translating scratchpads to full hierarchy..."
+./itis2.pl -o f -r ../mysql/output/scratchpads.tab > output/scratchpads_fullhierarchy.txt
+echo "Translating scratchpads to unique paths..."
+./itis2.pl -o p ../mysql/output/scratchpads.tab > output/scratchpads_uniquepaths.txt
 
-./checkitis.pl -o f -r ../php/output/bryan_itis.tab > output/bryan_itis_fullhierarchy.txt
-./checkitis.pl -o p ../php/output/bryan_itis.tab > output/bryan_itis_uniquepaths.txt
+echo "Translating bryan to full hierarchy..."
+./itis2.pl -o f -r ../php/output/bryan.tab > output/bryan_fullhierarchy.txt
+echo "Translating bryan to unique paths..."
+./itis2.pl -o p ../php/output/bryan.tab > output/bryan_uniquepaths.txt
 
-./checkitis.pl -o f -r ../php/output/bryozone_itis.tab > output/bryozone_itis_fullhierarchy.txt
-./checkitis.pl -o p ../php/output/bryozone_itis.tab > output/bryozone_itis_uniquepaths.txt
+echo "Translating bryozone to full hierarchy..."
+./itis2.pl -o f -r ../php/output/bryozone.tab > output/bryozone_fullhierarchy.txt
+echo "Translating bryozone to unique paths..."
+./itis2.pl -o p ../php/output/bryozone.tab > output/bryozone_uniquepaths.txt
