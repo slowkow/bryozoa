b4eebda2dba5894d8ada5d448a3e83191d2f94d7
Kamil Slowikowski <kslowikowski@gmail.com>
2010-08-08 15:34:21 -0500

    rewritten, now scrapebock.pl outputs a valid ITIS-for-scratchpads format

--- scripts/perl/scrapebock.pl
+++ scripts/perl/scrapebock.pl
@@ -5,6 +5,36 @@ use URI;
 use lib "lib";
 use Web::Scraper;
 
+my %ranksort = (
+  'Kingdom'     =>  1,
+  'Subkingdom'  =>  2,
+  'Phylum'      =>  3,
+  'Subphylum'   =>  4,
+  'Superclass'  =>  5,
+  'Class'       =>  6,
+  'Subclass'    =>  7,
+  'Infraclass'  =>  8,
+  'Superorder'  =>  9,
+  'Order'       => 10,
+  'Suborder'    => 11,
+  'Infraorder'  => 12,
+  'Superfamily' => 13,
+  'Family'      => 14,
+  'Subfamily'   => 15,
+  'Tribe'       => 16,
+  'Subtribe'    => 17,
+  'Genus'       => 18,
+  'Subgenus'    => 19,
+  'Species'     => 20,
+  'Subspecies'  => 21
+);
+my %rranksort = reverse %ranksort;
+
+sub nextRank {
+  my $cr = shift;
+  return $rranksort{$ranksort{$cr} - 1};
+}
+
 my $scraper = scraper {
   process "h1,h2,h3,h4,h5,h6,ul", 'headers[]' => scraper {
     process "h1,h2,h3,h4,h5,h6", 'head' => 'TEXT';
@@ -13,39 +43,91 @@ my $scraper = scraper {
   result 'headers';
 };
 
+my $url = 'http://www.bryozoa.net/famsys.html';
 # scrape bock's website
-my $hierarchy = $scraper->scrape(URI->new("http://www.bryozoa.net/famsys.html"));
+my $scraped = $scraper->scrape(URI->new($url));
 #~ use YAML;
-#~ print Dump $hierarchy;
+#~ print Dump $scraped;
 #~ use Data::Dumper;
-#~ print Dumper($hierarchy);
+#~ print Dumper($scraped);
 
-my %tabs = (
-  'class'       => "",
-  'order'       => "\t",
-  'suborder'    => "\t\t",
-  'infraorder'  => "\t\t\t",
-  'superfamily' => "\t\t\t\t",
-  'family'      => "\t\t\t\t\t",
-  'genus'       => "\t\t"
-);
-
-# dump the hierarchy in a somewhat legible format
-foreach my $step (@{$hierarchy}) {
-  if ($step->{'head'}) {
-    $step->{'head'} =~ /^\s*(\w+)\s*(.*)$/;
-    next if not defined $tabs{lc $1};
-    print $tabs{lc $1} . $step->{'head'} . "\n";
+my %hierarchy;
+my %last_taxon;
+$last_taxon{'Phylum'} = 'Bryozoa';
+print "rank_name\tunit_name1\tparent_name\n";
+print "Phylum\tBryozoa\t\n";
+# dump the scraped elements in a somewhat legible format
+foreach my $element (@{$scraped}) {
+  if ($element->{'head'}) {
+    # when we encounter 'unplaced',
+    # the following taxa belong to the parent order
+    if ($element->{'head'} =~ /(?:unplaced|uncertain)/i) {
+      delete $last_taxon{'Suborder'};
+      delete $last_taxon{'Infraorder'};
+      delete $last_taxon{'Superfamily'};
+    }
+    
+    $element->{'head'} =~ /^\s*(\w+)\s*(\w+\b)/;
+    
+    my $rank = ucfirst lc $1;
+    my $name = ucfirst lc $2;
+    
+    next if not defined $ranksort{$rank};
+    
+    my $parentrank = nextRank($rank);
+    while (!$last_taxon{$parentrank}) {
+      $parentrank = nextRank($parentrank);
+    }
+    my $parent = $last_taxon{$parentrank};
+    
+    #push(@{$hierarchy{$parent}}, $name);
+    
+    print "$rank\t$name\t$parent\n";
+    
+    $last_taxon{$rank} = $name;
   }
-  elsif ($step->{'listitems'}) {
-    foreach my $listitem (@{$step->{'listitems'}}) {
-      $listitem =~ /^\s*(\w+)\s*(.*)$/;
-      if (not defined $tabs{lc $1}) {
-        print $tabs{'genus'} . "GENUS " . $listitem . "\n";
+  elsif ($element->{'listitems'}) {
+    foreach my $listitem (@{$element->{'listitems'}}) {
+      $listitem =~ /^\s*(\w+)(?:\s*(\w+\b))?/;
+      
+      my $rank = ucfirst lc $1;
+      my $name = ucfirst lc $2;
+      
+      # we have the genus name, not the rank name
+      if (not defined $ranksort{$rank}) {
+        $name = $rank;
+        $rank = 'Genus';
+        
+        my $parentrank = nextRank($rank);
+        while (!$last_taxon{$parentrank}) {
+          $parentrank = nextRank($parentrank);
+        }
+        my $parent = $last_taxon{$parentrank};
+        
+        print "Genus\t$name\t$parent\n";
       }
+      # we have the rank name
       else {
-        print $tabs{lc $1} . $listitem . "\n";
+        my $parentrank = nextRank($rank);
+        while (!$last_taxon{$parentrank}) {
+          $parentrank = nextRank($parentrank);
+        }
+        my $parent = $last_taxon{$parentrank};
+        
+        print "$rank\t$name\t$parent\n";
       }
     }
   }
 }
+
+# Phil Bock's ways of marking unplaced taxa:
+##################################################
+# Cyclostomatids with family placement uncertain
+# Unplaced Cryptostomids
+# Unplaced Cystoporid genera
+# Unplaced Trepostome genera
+# Unplaced Fenestrate genera
+# Unplaced Ctenostomates
+# Unplaced Anascan genera
+# Unplaced Ascophoran genera
+##################################################
